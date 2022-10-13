c1c422a92433b5114683f0776d90c7c447a646e1
osmangold <osmanahmeddahab@gmail.com>
2022-10-13 14:03:05 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,27 @@
 #!/bin/bash
+
 #include <stdio.h>
 
 
 
 /**
- 
- * main - Printing
- * Return: Always 0 (Successful)
-  */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
 
-puts("\" Programming is like building amaultigual puzzel");
+	  puts("\"Programming is like building a multilingual puzzle");
 
 	    return (0);
 
 }
+
+
