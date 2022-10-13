518f273f7cb9f2e238884b77c3dfefc0a0c1fc0a
osmangold <osmanahmeddahab@gmail.com>
2022-10-13 14:37:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 
 #include <stdio.h>
 
-
-
 /**
- *
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
@@ -15,7 +13,7 @@ int main(void)
 
 	  puts("\"Programming is like building a multilingual puzzle");
 
-	    return (0);
+	  return (0);
 
 }
 
