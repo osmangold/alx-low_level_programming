05a16bf1f7f71d01235ac84f24900ce93227c0a1
osmangold <osmanahmeddahab@gmail.com>
2022-10-14 13:54:45 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * return 0
+ */
+
+int main(void)
+
+{
+	char c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+
+}
